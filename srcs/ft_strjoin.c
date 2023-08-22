46cc56ebbcafff89af535435e3f49706c27b0277
hnoguchi <>
2023-08-22 10:39:21 +0900

    Refactor ft_strjoin.c

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,32 +6,54 @@
 /*   By: louisnop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 13:41:41 by louisnop          #+#    #+#             */
-/*   Updated: 2023/08/18 17:59:30 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/08/22 10:39:01 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+static size_t	join_size(char const *s1, char const *s2)
 {
-	char	*res;
-	int		i;
-	int		j;
-	int		size;
+	size_t	dstsize = 0;
+	size_t	s2_siz - 0;
 
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	if (!(res = malloc(sizeof(char) * size))) {
+	if (s1 != NULL) {
+		dstsize = ft_strlen(s1);
+	}
+	if (s2 != NULL) {
+		s2_siz = ft_strlen(s2);
+	}
+	if (s2_siz < (SIZE_MAX - dstsize)) {
+		dstsize += s2_siz + 1;
+	}
+	else {
+		dstsize = SIZE_MAX;
+	}
+	return (dstsize);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	dstsize = 0;
+	char	*dst = NULL;
+
+	if (s1 == NULL && s2 == NULL) {
+		return (ft_strdup(""));
+	}
+	dstsize = join_size(s1, s2);
+	dst = (char *)ft_calloc(dstsize, sizeof(char));
+	if (dst == NULL) {
 		return (NULL);
 	}
-	i = 0;
-	while (s1[i]) {
-		res[i] = s1[i];
-		i++;
+	if (s1 == NULL && s2 != NULL) {
+		(void)ft_strlcpy(dst, s2, dstsize);
 	}
-	j = 0;
-	while (s2[j]) {
-		res[i++] = s2[j++];
+	else if (s1 != NULL && s2 == NULL) {
+		(void)ft_strlcpy(dst, s1, dstsize);
 	}
-	res[i] = '\0';
-	return (res);
+	else {
+		(void)ft_strlcpy(dst, s1, dstsize);
+		(void)ft_strlcat(dst, s2, dstsize);
+	}
+	return (dst);
 }
